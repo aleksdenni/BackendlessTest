7ce88e1c2c5d3afb63c752800c4bf3ce5ca6e0a8
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2021-12-01 19:17:04 +0200

    change reverse

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,17 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Test {
 
-
     public static void reverse(Node node){
-
+        Node prev=null;
         for (int i=0;node!=null;i++){
             Node next = node.getNext();
-            if (i==0){
-                node.setNext(null);
-            }
+            node.setNext(prev);
+            prev=node;
             node=next;
-            if (i!=0){
-                next.setNext(node);
-            }
         }
+        node=prev;
 
         /*List<Node> nodeList = new ArrayList<>();
         for (int i=0 ; node!=null ; i++){
@@ -47,5 +41,6 @@ public class Test {
             }
         }
         test.reverse(nodes.get(0));
+        System.out.println();
     }
 }
